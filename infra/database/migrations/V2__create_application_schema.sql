6cda636afc0a31e63dd2bcc806023f1581d39379
Kiarash Delavar <139377614+kiarashdelavar@users.noreply.github.com>
2026-08-08 21:00:21 +0200

    E01-02 add infra/database/migrations/V2__create_application_schema.sql

---
+++ infra/database/migrations/V2__create_application_schema.sql
@@ -0,0 +1,4 @@
+-- Empty application namespace reserved for later versioned migrations.
+CREATE SCHEMA IF NOT EXISTS streetsherlock;
+COMMENT ON SCHEMA streetsherlock IS
+    'Authoritative StreetSherlock business schema; empty at E01-02.';
