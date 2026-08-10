abd9a608ae73031ec31f44357e608d02cc210a46
Kiarash Delavar <139377614+kiarashdelavar@users.noreply.github.com>
2026-08-10 13:08:35 +0200

    Test dev identity environment isolation

--- apps/api/src/test/java/nl/streetsherlock/ConfigurationFailureTest.java
+++ apps/api/src/test/java/nl/streetsherlock/ConfigurationFailureTest.java
@@ -17,4 +17,20 @@ class ConfigurationFailureTest {
                 .close())
                 .hasMessageContaining("Unable to bind");
     }
+
+    @Test
+    void demoEnvironmentCannotSilentlyUseLocalIdentity() {
+        assertThatThrownBy(() -> new SpringApplicationBuilder(StreetSherlockApplication.class)
+                .web(WebApplicationType.NONE)
+                .properties(
+                        "streetsherlock.environment=demo",
+                        "spring.security.oauth2.resourceserver.jwt.jwk-set-uri="
+                                + "http://127.0.0.1:8180/realms/streetsherlock-dev/"
+                                + "protocol/openid-connect/certs")
+                .run()
+                .close())
+                .hasRootCauseMessage(
+                        "Local dev identity is forbidden outside local/test; "
+                                + "configure OIDC_JWK_SET_URI");
+    }
 }
